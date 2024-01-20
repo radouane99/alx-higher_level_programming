7aa1b87565a0334c25930e7789cfa4a1b44caf0c
Radouane EL-ASRI <radouane.asri99@gmail.com>
2024-01-20 15:07:49 -0800

    python

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+# Radouane elasri <radouane@gmail.com>
+
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
