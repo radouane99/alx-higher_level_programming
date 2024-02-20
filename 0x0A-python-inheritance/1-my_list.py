594359951f3870a586268aec75e4e0ac38ea963c
Radouane EL ASRI <54957690+radouane99@users.noreply.github.com>
2024-02-20 00:22:42 +0000

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-===========================
-Module with class MyList
-===========================
+class Mylist
 """
 
 
 class MyList(list):
-    """Class with method print_sorted"""
-    pass
+    """MyList class
+    """
 
     def print_sorted(self):
-        """Methot that sorted a list"""
-
-        print(sorted(list(self)))
+        """print list
+        """
+        print(sorted(self))
